690c001a106d41dcdf066b228b52fb1e28048793
Gal Topper <galt@iguazio.com>
2020-06-15 14:11:05 +0300

    Deep imports.

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,4 +1,4 @@
-import storey
+from storey import build_flow, Source, Map, Filter, FlatMap, Reduce, FlowError
 
 
 class TestException(Exception):
@@ -19,12 +19,12 @@ class RaiseEx:
 
 
 def test_functional_flow():
-    controller = storey.build_flow([
-        storey.Source(),
-        storey.Map(lambda x: x + 1),
-        storey.Filter(lambda x: x < 3),
-        storey.FlatMap(lambda x: [x, x * 10]),
-        storey.Reduce(0, lambda acc, x: acc + x),
+    controller = build_flow([
+        Source(),
+        Map(lambda x: x + 1),
+        Filter(lambda x: x < 3),
+        FlatMap(lambda x: [x, x * 10]),
+        Reduce(0, lambda acc, x: acc + x),
     ]).run()
 
     for _ in range(100):
@@ -36,31 +36,31 @@ def test_functional_flow():
 
 
 def test_error_flow():
-    controller = storey.build_flow([
-        storey.Source(),
-        storey.Map(lambda x: x + 1),
-        storey.Map(RaiseEx(500).raise_ex),
-        storey.Reduce(0, lambda acc, x: acc + x),
+    controller = build_flow([
+        Source(),
+        Map(lambda x: x + 1),
+        Map(RaiseEx(500).raise_ex),
+        Reduce(0, lambda acc, x: acc + x),
     ]).run()
 
     try:
         for i in range(1000):
             controller.emit(i)
-    except storey.FlowError as flow_ex:
+    except FlowError as flow_ex:
         assert isinstance(flow_ex.__cause__, TestException)
 
 
 def test_broadcast():
-    controller = storey.build_flow([
-        storey.Source(),
-        storey.Map(lambda x: x + 1),
-        storey.Filter(
+    controller = build_flow([
+        Source(),
+        Map(lambda x: x + 1),
+        Filter(
             lambda x: x < 3, termination_result_fn=lambda x, y: x + y),
         [
-            storey.Reduce(0, lambda acc, x: acc + x)
+            Reduce(0, lambda acc, x: acc + x)
         ],
         [
-            storey.Reduce(0, lambda acc, x: acc + x)
+            Reduce(0, lambda acc, x: acc + x)
         ]
     ]).run()
 
@@ -72,21 +72,21 @@ def test_broadcast():
 
 
 def test_broadcast_complex():
-    controller = storey.build_flow([
-        storey.Source(),
-        storey.Map(lambda x: x + 1),
-        storey.Filter(
+    controller = build_flow([
+        Source(),
+        Map(lambda x: x + 1),
+        Filter(
             lambda x: x < 3, termination_result_fn=lambda x, y: x + y),
         [
-            storey.Reduce(0, lambda acc, x: acc + x),
+            Reduce(0, lambda acc, x: acc + x),
         ],
         [
-            storey.Map(lambda x: x * 100),
-            storey.Reduce(0, lambda acc, x: acc + x)
+            Map(lambda x: x * 100),
+            Reduce(0, lambda acc, x: acc + x)
         ],
         [
-            storey.Map(lambda x: x * 1000),
-            storey.Reduce(0, lambda acc, x: acc + x)
+            Map(lambda x: x * 1000),
+            Reduce(0, lambda acc, x: acc + x)
         ]
     ]).run()
 
@@ -99,13 +99,13 @@ def test_broadcast_complex():
 
 # Same as test_broadcast_complex but without using build_flow
 def test_broadcast_complex_no_sugar():
-    source = storey.Source()
-    filter = storey.Filter(
+    source = Source()
+    filter = Filter(
         lambda x: x < 3, termination_result_fn=lambda x, y: x + y)
-    source.to(storey.Map(lambda x: x + 1)).to(filter)
-    filter.to(storey.Reduce(0, lambda acc, x: acc + x), )
-    filter.to(storey.Map(lambda x: x * 100)).to(storey.Reduce(0, lambda acc, x: acc + x))
-    filter.to(storey.Map(lambda x: x * 1000)).to(storey.Reduce(0, lambda acc, x: acc + x))
+    source.to(Map(lambda x: x + 1)).to(filter)
+    filter.to(Reduce(0, lambda acc, x: acc + x), )
+    filter.to(Map(lambda x: x * 100)).to(Reduce(0, lambda acc, x: acc + x))
+    filter.to(Map(lambda x: x * 1000)).to(Reduce(0, lambda acc, x: acc + x))
     controller = source.run()
 
     for i in range(10):
--- tests/test_flow_integration.py
+++ tests/test_flow_integration.py
@@ -1,13 +1,13 @@
-import storey
+from storey import build_flow, Source, Map, Filter, JoinWithTable
 
 
 def test_functional_flow():
-    flow = storey.build_flow([
-        storey.Source(),
-        storey.Map(lambda x: x + 1),
-        storey.Filter(lambda x: x < 3),
-        storey.JoinWithTable(lambda x: x, lambda x, y: y['secret'], '/bigdata/gal'),
-        storey.Map(lambda x: print(x))
+    flow = build_flow([
+        Source(),
+        Map(lambda x: x + 1),
+        Filter(lambda x: x < 3),
+        JoinWithTable(lambda x: x, lambda x, y: y['secret'], '/bigdata/gal'),
+        Map(lambda x: print(x))
     ])
     mat = flow.run()
     for _ in range(100):
