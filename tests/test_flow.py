324e34bcf0808a215f489ffb60400e2f124ee7c6
Miki Tebeka <miki.tebeka@gmail.com>
2020-06-15 13:55:27 +0300

    pytest

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -1,6 +1,4 @@
-import unittest
-
-from storey import *
+import storey
 
 
 class TestException(Exception):
@@ -20,96 +18,98 @@ class RaiseEx:
         return element
 
 
-class TestFlow(unittest.TestCase):
-    def test_functional_flow(self):
-        controller = build_flow([
-            Source(),
-            Map(lambda x: x + 1),
-            Filter(lambda x: x < 3),
-            FlatMap(lambda x: [x, x * 10]),
-            Reduce(0, lambda acc, x: acc + x),
-        ]).run()
+def test_functional_flow():
+    controller = storey.build_flow([
+        storey.Source(),
+        storey.Map(lambda x: x + 1),
+        storey.Filter(lambda x: x < 3),
+        storey.FlatMap(lambda x: [x, x * 10]),
+        storey.Reduce(0, lambda acc, x: acc + x),
+    ]).run()
 
-        for _ in range(100):
-            for i in range(10):
-                controller.emit(i)
-        controller.terminate()
-        termination_result = controller.await_termination()
-        self.assertEqual(3300, termination_result)
-
-    def test_error_flow(self):
-        controller = build_flow([
-            Source(),
-            Map(lambda x: x + 1),
-            Map(RaiseEx(500).raise_ex),
-            Reduce(0, lambda acc, x: acc + x),
-        ]).run()
-
-        try:
-            for i in range(1000):
-                controller.emit(i)
-        except FlowError as flow_ex:
-            self.assertEqual(TestException, type(flow_ex.__cause__))
-
-    def test_broadcast(self):
-        controller = build_flow([
-            Source(),
-            Map(lambda x: x + 1),
-            Filter(lambda x: x < 3, termination_result_fn=lambda x, y: x + y),
-            [
-                Reduce(0, lambda acc, x: acc + x)
-            ],
-            [
-                Reduce(0, lambda acc, x: acc + x)
-            ]
-        ]).run()
-
+    for _ in range(100):
         for i in range(10):
             controller.emit(i)
-        controller.terminate()
-        termination_result = controller.await_termination()
-        self.assertEqual(6, termination_result)
+    controller.terminate()
+    termination_result = controller.await_termination()
+    assert 3300 == termination_result
 
-    def test_broadcast_complex(self):
-        controller = build_flow([
-            Source(),
-            Map(lambda x: x + 1),
-            Filter(lambda x: x < 3, termination_result_fn=lambda x, y: x + y),
-            [
-                Reduce(0, lambda acc, x: acc + x),
-            ],
-            [
-                Map(lambda x: x * 100),
-                Reduce(0, lambda acc, x: acc + x)
-            ],
-            [
-                Map(lambda x: x * 1000),
-                Reduce(0, lambda acc, x: acc + x)
-            ]
-        ]).run()
 
-        for i in range(10):
+def test_error_flow():
+    controller = storey.build_flow([
+        storey.Source(),
+        storey.Map(lambda x: x + 1),
+        storey.Map(RaiseEx(500).raise_ex),
+        storey.Reduce(0, lambda acc, x: acc + x),
+    ]).run()
+
+    try:
+        for i in range(1000):
             controller.emit(i)
-        controller.terminate()
-        termination_result = controller.await_termination()
-        self.assertEqual(3303, termination_result)
-
-    # Same as test_broadcast_complex but without using build_flow
-    def test_broadcast_complex_no_sugar(self):
-        source = Source()
-        filter = Filter(lambda x: x < 3, termination_result_fn=lambda x, y: x + y)
-        source.to(Map(lambda x: x + 1)).to(filter)
-        filter.to(Reduce(0, lambda acc, x: acc + x), )
-        filter.to(Map(lambda x: x * 100)).to(Reduce(0, lambda acc, x: acc + x))
-        filter.to(Map(lambda x: x * 1000)).to(Reduce(0, lambda acc, x: acc + x))
-        controller = source.run()
-
-        for i in range(10):
-            controller.emit(i)
-        controller.terminate()
-        termination_result = controller.await_termination()
-        self.assertEqual(3303, termination_result)
+    except storey.FlowError as flow_ex:
+        assert isinstance(flow_ex.__cause__, TestException)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_broadcast():
+    controller = storey.build_flow([
+        storey.Source(),
+        storey.Map(lambda x: x + 1),
+        storey.Filter(
+            lambda x: x < 3, termination_result_fn=lambda x, y: x + y),
+        [
+            storey.Reduce(0, lambda acc, x: acc + x)
+        ],
+        [
+            storey.Reduce(0, lambda acc, x: acc + x)
+        ]
+    ]).run()
+
+    for i in range(10):
+        controller.emit(i)
+    controller.terminate()
+    termination_result = controller.await_termination()
+    assert 6 == termination_result
+
+
+def test_broadcast_complex():
+    controller = storey.build_flow([
+        storey.Source(),
+        storey.Map(lambda x: x + 1),
+        storey.Filter(
+            lambda x: x < 3, termination_result_fn=lambda x, y: x + y),
+        [
+            storey.Reduce(0, lambda acc, x: acc + x),
+        ],
+        [
+            storey.Map(lambda x: x * 100),
+            storey.Reduce(0, lambda acc, x: acc + x)
+        ],
+        [
+            storey.Map(lambda x: x * 1000),
+            storey.Reduce(0, lambda acc, x: acc + x)
+        ]
+    ]).run()
+
+    for i in range(10):
+        controller.emit(i)
+    controller.terminate()
+    termination_result = controller.await_termination()
+    assert 3303 == termination_result
+
+
+# Same as test_broadcast_complex but without using build_flow
+def test_broadcast_complex_no_sugar():
+    source = storey.Source()
+    filter = storey.Filter(
+        lambda x: x < 3, termination_result_fn=lambda x, y: x + y)
+    source.to(storey.Map(lambda x: x + 1)).to(filter)
+    filter.to(storey.Reduce(0, lambda acc, x: acc + x), )
+    filter.to(storey.Map(lambda x: x * 100)).to(storey.Reduce(0, lambda acc, x: acc + x))
+    filter.to(storey.Map(lambda x: x * 1000)).to(storey.Reduce(0, lambda acc, x: acc + x))
+    controller = source.run()
+
+    for i in range(10):
+        controller.emit(i)
+    controller.terminate()
+    termination_result = controller.await_termination()
+    assert 3303 == termination_result
--- tests/test_flow_integration.py
+++ tests/test_flow_integration.py
@@ -1,24 +1,17 @@
-import unittest
-
-from storey import *
+import storey
 
 
-class TestJoinWithTable(unittest.TestCase):
-    def test_functional_flow(self):
-        flow = build_flow([
-            Source(),
-            Map(lambda x: x + 1),
-            Filter(lambda x: x < 3),
-            JoinWithTable(lambda x: x, lambda x, y: y['secret'], '/bigdata/gal'),
-            Map(lambda x: print(x))
-        ])
-        mat = flow.run()
-        for _ in range(100):
-            for i in range(10):
-                mat.emit(i)
-        mat.terminate()
-        mat.await_termination()
-
-
-if __name__ == '__main__':
-    unittest.main()
+def test_functional_flow():
+    flow = storey.build_flow([
+        storey.Source(),
+        storey.Map(lambda x: x + 1),
+        storey.Filter(lambda x: x < 3),
+        storey.JoinWithTable(lambda x: x, lambda x, y: y['secret'], '/bigdata/gal'),
+        storey.Map(lambda x: print(x))
+    ])
+    mat = flow.run()
+    for _ in range(100):
+        for i in range(10):
+            mat.emit(i)
+    mat.terminate()
+    mat.await_termination()
