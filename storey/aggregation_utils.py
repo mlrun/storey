47883a7fa7c365da00415070e4d227afd70eaaa6
Tal Neiman <33829179+talIguaz@users.noreply.github.com>
2020-09-14 16:50:56 +0300

    Persist aggregations (#4)
    * add basic persistance of schema+store to v3io + fix not showing hidden aggr
    
    * move persistance logic to V3ioTable
    
    * use a mock table for aggregation tests
    
    * update schema when needed even though it exists
    
    * merge schemas
    
    * add teardown to tests + minor changes
    
    * add persist by key
    
    * fix lint
    
    * fix tests
    
    * add container to test table
    
    * review comments - dont expose V3ioTable, remove redundant stuff, create session only once
    
    * lint - format flow.py
    
    * persist step, new Cache object that contains storage driver
    
    * merge fixes
    
    * make an opaque storage handle, and other minor review comments
    
    * remove unneeded code
    
    * review comments
    
    * close connection
    
    * rename dependant_aggr -> implied, add schmea merge tests, other review comments
    
    * fix nginx-py conversion + other review comments
    
    * fix test
    
    * await clost connection in tests
    
    Co-authored-by: Gal Topper <galt@iguazio.com>

--- storey/aggregation_utils.py
+++ storey/aggregation_utils.py
@@ -80,10 +80,28 @@ def get_virtual_aggregation_func(aggregation):
     raise TypeError(f'"{aggregation}" aggregator is not defined')
 
 
-def get_dependant_aggregates(aggregate):
+def get_implied_aggregates(aggregate):
     aggrs = []
     aggr_bits = _all_aggregates_by_name[aggregate]
     for raw_aggr in _raw_aggregates:
         if aggr_bits & raw_aggr == raw_aggr:
             aggrs.append(_all_aggregates_to_name[raw_aggr])
     return aggrs
+
+
+def get_all_raw_aggregates_with_hidden(aggregates):
+    raw_aggregates = {}
+
+    for aggregate in aggregates:
+        if is_raw_aggregate(aggregate):
+            raw_aggregates[aggregate] = False
+        else:
+            for dependant_aggr in get_implied_aggregates(aggregate):
+                if dependant_aggr not in raw_aggregates:
+                    raw_aggregates[dependant_aggr] = True
+
+    return raw_aggregates
+
+
+def get_all_raw_aggregates(aggregates):
+    return set(get_all_raw_aggregates_with_hidden(aggregates).keys())
