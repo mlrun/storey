78ffe6fba3d9cff23237b849189a24bc7de2156e
Tal Neiman <33829179+talIguaz@users.noreply.github.com>
2020-09-16 11:22:04 +0300

    Add Batch step (#5)
    * add Batch step
    
    * review comments
    
    * cancell Batch task on termination event

--- storey/flow.py
+++ storey/flow.py
@@ -649,6 +649,49 @@ class JoinWithHttp(Flow):
                 await self._worker_awaitable
 
 
+class Batch(Flow):
+    def __init__(self, max_events, timeout_secs=None, **kwargs):
+        Flow.__init__(self, **kwargs)
+        self._max_events = max_events
+        self._event_count = 0
+        self._batch = []
+        self._timeout_task = None
+
+        self._timeout_secs = timeout_secs
+        if self._timeout_secs <= 0:
+            raise ValueError('Batch timeout cannot be 0 or negative')
+
+    async def sleep_and_emit(self):
+        await asyncio.sleep(self._timeout_secs)
+        await self.emit_batch()
+
+    async def _do(self, event):
+        if event is _termination_obj:
+            if self._timeout_task and not self._timeout_task.cancelled():
+                self._timeout_task.cancel()
+            await self.emit_batch()
+            return await self._do_downstream(_termination_obj)
+        else:
+            if len(self._batch) == 0 and self._timeout_secs:
+                self._timeout_task = asyncio.get_running_loop().create_task(self.sleep_and_emit())
+
+            self._event_count = self._event_count + 1
+            self._batch.append(event)
+
+            if self._event_count == self._max_events:
+                if self._timeout_task and not self._timeout_task.cancelled():
+                    self._timeout_task.cancel()
+                await self.emit_batch()
+
+    async def emit_batch(self):
+        if len(self._batch) > 0:
+            batch_to_emit = self._batch
+            self._batch = []
+            self._event_count = 0
+
+            await self._do_downstream(Event([self._get_safe_event_or_body(event) for event in batch_to_emit], time=batch_to_emit[0].time))
+
+
 _non_int_char_pattern = re.compile(r"[^-0-9]")
 
 
