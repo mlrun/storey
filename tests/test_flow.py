52964c12f9acdd2070e6a7e9927a427bd9bd9c79
Gal Topper <galt@iguaz.io>
2020-12-17 10:40:09 +0200

    Re-add NoopDriver. (#107)
    Co-authored-by: Gal Topper <galt@iguazio.com>

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -8,7 +8,7 @@ from aiohttp import InvalidURL
 
 from storey import build_flow, Source, Map, Filter, FlatMap, Reduce, FlowError, MapWithState, ReadCSV, Complete, AsyncSource, Choice, \
     Event, Batch, Table, WriteToCSV, DataframeSource, MapClass, JoinWithTable, ReduceToDataFrame, ToDataFrame, WriteToParquet, \
-    WriteToTSDB, Extend, SendToHttp, HttpRequest, WriteToTable, Driver
+    WriteToTSDB, Extend, SendToHttp, HttpRequest, WriteToTable, NoopDriver, Driver
 
 
 class ATestException(Exception):
@@ -310,7 +310,7 @@ def test_map_with_state_flow():
 
 
 def test_map_with_cache_state_flow():
-    table_object = Table("table", Driver())
+    table_object = Table("table", NoopDriver())
     table_object._cache['tal'] = {'color': 'blue'}
     table_object._cache['dina'] = {'color': 'red'}
 
@@ -350,7 +350,7 @@ def test_map_with_cache_state_flow():
 
 
 def test_map_with_empty_cache_state_flow():
-    table_object = Table("table", Driver())
+    table_object = Table("table", NoopDriver())
 
     def enrich(event, state):
         if 'first_value' not in state:
@@ -1162,7 +1162,7 @@ def test_write_to_parquet_with_inference(tmpdir):
 
 
 def test_join_by_key():
-    table = Table('test', Driver())
+    table = Table('test', NoopDriver())
     table.update_key(9, {'age': 1, 'color': 'blue9'})
     table.update_key(7, {'age': 3, 'color': 'blue7'})
 
@@ -1182,7 +1182,7 @@ def test_join_by_key():
 
 
 def test_join_by_string_key():
-    table = Table('test', Driver())
+    table = Table('test', NoopDriver())
     table.update_key(9, {'age': 1, 'color': 'blue9'})
     table.update_key(7, {'age': 3, 'color': 'blue7'})
 
