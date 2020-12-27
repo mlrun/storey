eff49c1c94e5866dcdee0119c84365638408f0fe
Gal Topper <galt@iguazio.com>
2020-12-27 13:59:55 +0200

    Add set-version.py

---
+++ set-version.py
@@ -0,0 +1,20 @@
+from os import environ
+
+
+def set_version():
+    version = environ.get('GITHUB_REF')
+    assert version, 'GITHUB_REF is not defined'
+
+    lines = []
+    init_py = 'storey/__init__.py'
+    with open(init_py) as fp:
+        for line in fp:
+            if '__version__' in line:
+                line = "__version__ = '{}'\n".format(version)
+            lines.append(line)
+
+    with open(init_py, 'w') as out:
+        out.write(''.join(lines))
+
+
+set_version()
