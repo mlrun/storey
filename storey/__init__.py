3b45177abb7d77514cb122377c53523cc11e1080
Tal Neiman <Taln@iguazio.com>
2020-12-29 13:28:54 +0200

    fix lint

--- storey/__init__.py
+++ storey/__init__.py
@@ -2,7 +2,6 @@ __version__ = '0.2.0'
 
 # Importing v3iofs explicitly so that it will get registered as an fsspec filesystem
 import v3iofs  # noqa: F401
-del v3iofs  # clear the module namespace
 
 from .aggregations import (  # noqa: F401
     AggregateByKey, QueryByKey
@@ -29,3 +28,5 @@ from .table import (  # noqa: F401
 from .writers import (  # noqa: F401
     WriteToV3IOStream, WriteToCSV, WriteToParquet, WriteToTSDB, WriteToTable
 )
+
+del v3iofs  # clear the module namespace
