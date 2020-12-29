7b1278c98584073da9d1fb454d87d6b4095b29f3
Tal Neiman <Taln@iguazio.com>
2020-12-29 13:23:10 +0200

    add comment and delete v3iofs from namesapce

--- storey/__init__.py
+++ storey/__init__.py
@@ -1,6 +1,9 @@
 __version__ = '0.2.0'
 
+# Importing v3iofs explicitly so that it will get registered as an fsspec filesystem
 import v3iofs  # noqa: F401
+del v3iofs  # clear the module namespace
+
 from .aggregations import (  # noqa: F401
     AggregateByKey, QueryByKey
 )
