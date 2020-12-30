4ac355bbfc20415c03ee72547c5064daa819e925
Gal Topper <galt@iguazio.com>
2020-12-30 14:48:14 +0200

    Fix the fix.

--- set-version.py
+++ set-version.py
@@ -5,14 +5,14 @@ def set_version():
     version = environ.get('GITHUB_REF')
     assert version, 'GITHUB_REF is not defined'
 
-    version = version.replace('refs-tags-v', '')
+    version = version.replace('refs/tags/v', '')
 
     lines = []
     init_py = 'storey/__init__.py'
     with open(init_py) as fp:
         for line in fp:
             if '__version__' in line:
-                line = "__version__ = '{}'\n".format(version)
+                line = f"__version__ = '{version}'\n"
             lines.append(line)
 
     with open(init_py, 'w') as out:
