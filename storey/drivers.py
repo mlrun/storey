4b37da5b733e5ccb701ad2086b4fce0aaa533470
Gal Topper <galt@iguazio.com>
2021-04-20 11:50:24 +0300

    Replace dict with set.

--- storey/drivers.py
+++ storey/drivers.py
@@ -93,14 +93,10 @@ class V3ioDriver(NeedsV3ioAccess, Driver):
             await self._v3io_client.close()
             self._v3io_client = None
 
-    saved_engine_words = {'min': True, 'max': True, 'sqrt': True, 'avg': True, 'stddev': True, 'sum': True,
-                          'length': True, 'init_array': True, 'set': True, 'remove': True,
-                          'regexp_replace': True, 'find': True, 'find_all_indices': True,
-                          'extract_by_indices': True, 'sort_array': True, 'blob': True, 'if_else': True,
-                          'in': True, 'true': True, 'false': True, 'and': True, 'or': True, 'not': True,
-                          'contains': True, 'ends': True, 'starts': True, 'exists': True, 'select': True,
-                          'str': True, 'to_int': True, 'if_not_exists': True, 'regexp_instr': True, 'delete': True,
-                          'as': True, 'maxdbl': True, 'inf': True, 'nan': True, 'isnan': True, 'nanaszero': True}
+    saved_engine_words = {'min', 'max', 'sqrt', 'avg', 'stddev', 'sum', 'length', 'init_array', 'set', 'remove', 'regexp_replace', 'find',
+                          'find_all_indices', 'extract_by_indices', 'sort_array', 'blob', 'if_else', 'in', 'true', 'false', 'and', 'or',
+                          'not', 'contains', 'ends', 'starts', 'exists', 'select', 'str', 'to_int', 'if_not_exists', 'regexp_instr',
+                          'delete', 'as', 'maxdbl', 'inf', 'nan', 'isnan', 'nanaszero'}
 
     async def _save_schema(self, container, table_path, schema):
         self._lazy_init()
@@ -220,7 +216,7 @@ class V3ioDriver(NeedsV3ioAccess, Driver):
         # Generating additional cache expressions
         if additional_data:
             for name, value in additional_data.items():
-                if name.casefold() in self.saved_engine_words.keys():
+                if name.casefold() in self.saved_engine_words:
                     name = f'`{name}`'
                 expression_value = self._convert_python_obj_to_expression_value(value)
                 if expression_value:
