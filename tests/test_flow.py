05c1dc8773f6898b613462735f8f07d88aeab610
katyak <katyak@iguazio.com>
2021-06-07 15:29:10 +0300

    PR comments

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -143,10 +143,10 @@ def test_csv_reader_as_dict_with_key_and_timestamp():
     termination_result = controller.await_termination()
 
     assert len(termination_result) == 2
-    assert termination_result[0].key == ['m1']
+    assert termination_result[0].key == 'm1'
     assert termination_result[0].time == datetime(2020, 2, 15, 2, 0)
     assert termination_result[0].body == {'k': 'm1', 't': datetime(2020, 2, 15, 2, 0), 'v': 8, 'b': True}
-    assert termination_result[1].key == ['m2']
+    assert termination_result[1].key == 'm2'
     assert termination_result[1].time == datetime(2020, 2, 16, 2, 0)
     assert termination_result[1].body == {'k': 'm2', 't': datetime(2020, 2, 16, 2, 0), 'v': 14, 'b': False}
 
@@ -178,10 +178,10 @@ def test_csv_reader_with_key_and_timestamp():
     termination_result = controller.await_termination()
 
     assert len(termination_result) == 2
-    assert termination_result[0].key == ['m1']
+    assert termination_result[0].key == 'm1'
     assert termination_result[0].time == datetime(2020, 2, 15, 2, 0)
     assert termination_result[0].body == ['m1', datetime(2020, 2, 15, 2, 0), 8, True]
-    assert termination_result[1].key == ['m2']
+    assert termination_result[1].key == 'm2'
     assert termination_result[1].time == datetime(2020, 2, 16, 2, 0)
     assert termination_result[1].body == ['m2', datetime(2020, 2, 16, 2, 0), 14, False]
 
@@ -2490,9 +2490,9 @@ def test_csv_reader_with_none_values():
     print(termination_result)
 
     assert len(termination_result) == 2
-    assert termination_result[0].key == ['a']
+    assert termination_result[0].key == 'a'
     assert termination_result[0].body == ['a', True, False, 1, 2.3, datetime(2021, 4, 21, 15, 56, 53, 385444)]
-    assert termination_result[1].key == ['b']
+    assert termination_result[1].key == 'b'
     assert termination_result[1].body == ['b', True, None, math.nan, math.nan, None]
 
 
