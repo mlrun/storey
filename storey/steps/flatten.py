bca3472a9ea84da07163541c5e30009bbb056036
Michael Livshits <livsmichael@gmail.com>
2021-06-16 12:18:40 +0300

    Made sure Flatten forces full_event=False to ensure we can iterate event body

--- storey/steps/flatten.py
+++ storey/steps/flatten.py
@@ -2,4 +2,7 @@ from storey import FlatMap
 
 
 def Flatten(**kwargs):
+    # Please note that Flatten forces full_event=False, since otherwise we can't iterate the body of the event
+    if kwargs:
+        kwargs["full_event"] = False
     return FlatMap(lambda x: x, **kwargs)
