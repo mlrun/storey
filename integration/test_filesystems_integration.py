59383947238b077d090a2146dc88e90696f2107a
Gal Topper <galt@iguazio.com>
2021-06-24 12:02:44 +0300

    Allow pyarrow 3.x. Adjust tests. (#250)
    * Add pyarrow 3 compatibility to tests.
    
    * Allow pyarrow 1.x to 3.x.

--- integration/test_filesystems_integration.py
+++ integration/test_filesystems_integration.py
@@ -205,12 +205,16 @@ def test_write_to_parquet_to_v3io(setup_teardown_test):
     for i in range(10):
         controller.emit([i, f'this is {i}'])
         expected.append([i, f'this is {i}'])
-    expected = pd.DataFrame(expected, columns=columns, dtype='int32')
+    expected_in_pyarrow1 = pd.DataFrame(expected, columns=columns)
+    expected_in_pyarrow3 = expected_in_pyarrow1.copy()
+    expected_in_pyarrow1['my_int'] = expected_in_pyarrow1['my_int'].astype('int32')
+    expected_in_pyarrow3['my_int'] = expected_in_pyarrow3['my_int'].astype('category')
+
     controller.terminate()
     controller.await_termination()
 
     read_back_df = pd.read_parquet(out_dir, columns=columns)
-    assert read_back_df.equals(expected), f"{read_back_df}\n!=\n{expected}"
+    assert read_back_df.equals(expected_in_pyarrow1) or read_back_df.equals(expected_in_pyarrow3)
 
 
 def test_write_to_parquet_to_v3io_single_file_on_termination(setup_teardown_test):
