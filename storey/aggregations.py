9cc5befc279a9a49b8e23f167606a7d5459f68c3
Eyal Salomon <eyals@iguazio.com>
2021-08-01 10:15:52 +0300

    [Storey] Using table string in `QueryByKey` fails (#276)
    Signed-off-by: Eyal Salomon <eyals@iguazio.com>

--- storey/aggregations.py
+++ storey/aggregations.py
@@ -254,7 +254,10 @@ class QueryByKey(AggregateByKey):
             feature, aggr = name.rsplit('_', 1)
             # setting as SlidingWindow temporarily until actual window type will be read from schema
             self._aggrs.append(FieldAggregator(name=feature, field=None, aggr=[aggr], windows=SlidingWindows(windows, '10m')))
-        other_table = table._clone() if table._aggregates is not None else table
+        if isinstance(table, Table):
+            other_table = table._clone() if table._aggregates is not None else table
+        else:
+            other_table = table   # str - pass table string along with the context object
         AggregateByKey.__init__(self, self._aggrs, other_table, key, augmentation_fn=augmentation_fn,
                                 enrich_with=self._enrich_cols, aliases=aliases, use_windows_from_schema=True, **kwargs)
         self._table._aggregations_read_only = True
