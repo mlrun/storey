6322181559f4fa10221a021672c99690bcccde0c
davesh0812 <davesh0812@gmail.com>
2022-06-15 15:58:33 +0300

    mongodb target init

--- storey/targets.py
+++ storey/targets.py
@@ -927,5 +927,4 @@ class MongoDBTarget(Flow, _Writer):
             return await self._do_downstream(_termination_obj)
         else:
             record = self._event_to_writer_entry(event)
-            record = json.dumps(record).encode("UTF-8")
             self._my_collection.insert_one(record)
