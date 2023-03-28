cb37cedd39f24766a057cd65bcbb778e6c7a68d8
davesh0812 <davesh0812@gmail.com>
2023-03-28 14:11:22 +0300

    lint

--- storey/sql_driver.py
+++ storey/sql_driver.py
@@ -29,14 +29,22 @@ class SQLDriver(Driver):
     :param time_fields: list of all fields that are timestamps
     """
 
-    def __init__(self, primary_key: Union[str, List[str]], db_path: str, time_fields: List[str] = None):
+    def __init__(
+        self,
+        primary_key: Union[str, List[str]],
+        db_path: str,
+        time_fields: List[str] = None,
+    ):
         self._db_path = db_path
         self._sql_connection = None
-        self._primary_key = primary_key if isinstance(primary_key, list) else self._extract_list_of_keys(primary_key)
+        self._primary_key = (
+            primary_key
+            if isinstance(primary_key, list)
+            else self._extract_list_of_keys(primary_key)
+        )
         self._time_fields = time_fields
 
     def _lazy_init(self):
-
         if not self._sql_connection:
             self._engine = db.create_engine(self._db_path)
             self._sql_connection = self._engine.connect()
@@ -51,7 +59,9 @@ class SQLDriver(Driver):
             autoload_with=self._engine,
         )
 
-    async def _save_key(self, container, table_path, key, aggr_item, partitioned_by_key, additional_data):
+    async def _save_key(
+        self, container, table_path, key, aggr_item, partitioned_by_key, additional_data
+    ):
         self._lazy_init()
         key = self._extract_list_of_keys(key)
         for i in range(len(self._primary_key)):
@@ -59,7 +69,9 @@ class SQLDriver(Driver):
         table = self._table(table_path)
         df = pd.DataFrame(additional_data, index=[0])
         try:
-            df.to_sql(table.name, con=self._sql_connection, if_exists="append", index=False)
+            df.to_sql(
+                table.name, con=self._sql_connection, if_exists="append", index=False
+            )
         except db.exc.IntegrityError:
             self._update_by_key(key, additional_data, table)
 
@@ -89,7 +101,9 @@ class SQLDriver(Driver):
     async def _get_all_fields(self, key, table):
         where_clause = self._get_where_clause(key, table)
         query = f"SELECT * FROM {table} where {where_clause}"
-        results = pd.read_sql(query, con=self._sql_connection, parse_dates=self._time_fields).to_dict(orient="records")
+        results = pd.read_sql(
+            query, con=self._sql_connection, parse_dates=self._time_fields
+        ).to_dict(orient="records")
 
         return results[0]
 
@@ -97,9 +111,9 @@ class SQLDriver(Driver):
         where_clause = self._get_where_clause(key, table)
         try:
             query = f"SELECT {','.join(attributes)} FROM {table} as {table.name} where {where_clause}"
-            results = pd.read_sql(query, con=self._sql_connection, parse_dates=self._time_fields).to_dict(
-                orient="records"
-            )
+            results = pd.read_sql(
+                query, con=self._sql_connection, parse_dates=self._time_fields
+            ).to_dict(orient="records")
         except Exception as e:
             raise RuntimeError(f"Failed to get key '{key}'") from e
 
@@ -122,8 +136,16 @@ class SQLDriver(Driver):
 
     def _update_by_key(self, key, data, table):
         where_clause = self._get_where_clause(key, table)
-        update_clause = " ,".join([f'{table.name}.{key}="{value}"' for key, value in data.items() if key not in self._primary_key])
-        sql_statement = f"UPDATE {table} as {table.name} SET {update_clause} where {where_clause}"
+        update_clause = " ,".join(
+            [
+                f'{table.name}.{key}="{value}"'
+                for key, value in data.items()
+                if key not in self._primary_key
+            ]
+        )
+        sql_statement = (
+            f"UPDATE {table} as {table.name} SET {update_clause} where {where_clause}"
+        )
         self._sql_connection.execute(sql_statement)
 
     @staticmethod
