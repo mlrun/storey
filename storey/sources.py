a1b3fea79eace2cd81cb3d312782d694c87461af
tomermamia <tomerm@iguazio.com>
2023-04-03 15:55:19 +0300

    built is_nan_validator

--- storey/sources.py
+++ storey/sources.py
@@ -1084,9 +1084,8 @@ class CSVSource(DataframeSource):
             else:
                 # TODO change error messge.
                 raise self.NoneKeyException(f"For {body} value of {field_type} {field} is None")
-        if pandas.isna(result) or result is None:
-            if raise_exception:
-                raise self.NoneKeyException(f"For {body} value of {field_type} {field} is None")
+        if raise_exception:
+            self.is_nan_validator(result=result, body=body, field_type=field_type, field=field)
         return result
 
 
