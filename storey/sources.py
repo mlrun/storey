e6535fe6ee9869709c9021de46506ffac0499b63
tomermamia <tomerm@iguazio.com>
2023-04-16 14:30:39 +0300

    add field_validator to csv

--- storey/sources.py
+++ storey/sources.py
@@ -826,7 +826,22 @@ class CSVSource(DataframeSource):
                 self.is_nan_validator(result=result, body=body, field_type=field_type, field=field)
         return result
 
+    def field_validator(self, df, key_field, id_field, path='file path was not provided.'):
+        str_key_field = []
+        str_id_field = None
+        if id_field is not None:
+            if isinstance(id_field, str):
+                str_id_field = id_field
+            elif isinstance(id_field, int) and (id_field<0 or id_field>=len(df.columns)):
+                raise IndexError(f'IndexError: id {id_field} is int and isn\'t in df index range. Df path: {path}')
+        if key_field:
+            str_key_field = [key for key in key_field if isinstance(key, str)]
+            int_key_field = [key for key in key_field if isinstance(key, int)]
+            out_of_range_keys = [int_key for int_key in int_key_field if int_key < 0 or int_key >= len(df.columns)]
+            if out_of_range_keys:
+                raise IndexError(f'IndexError: keys {out_of_range_keys} are int and isn\'t in df index range. Df path: {path}')
 
+        super().field_validator(df=df,key_field=str_key_field,id_field=str_id_field,path=path)
 class ParquetSource(DataframeSource):
     """Reads Parquet files as input source for a flow.
 
