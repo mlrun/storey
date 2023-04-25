5e00fec979c2c02f9c34765cb629337c6192aa37
tomermamia <tomerm@iguazio.com>
2023-04-25 11:14:24 +0300

    add build_dict check in _init_

--- storey/sources.py
+++ storey/sources.py
@@ -781,6 +781,8 @@ class CSVSource(DataframeSource):
                 isinstance(id_field, str)
         ):
             raise ValueError("id_field can only be set to an integer when with_header is false")
+        if build_dict and not header:
+            raise ValueError("build_dict can only be set to True when with_header is True")
         super().__init__([], **kwargs)
 
     def _init(self):
