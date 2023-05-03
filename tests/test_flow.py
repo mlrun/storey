e2bcc3e44d949bfdc5afed89101d856e637af798
Tomer_Mamia <tomerm@iguazio.com>
2023-05-03 12:13:15 +0300

    fix test_csv_reader_none_in_keyfield_should_send_error_log

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -424,7 +424,7 @@ def test_csv_reader_none_in_keyfield_should_send_error_log():
     controller.await_termination()
 
     assert "error" == logger.logs[0][0]
-    assert "value of key k is None" in logger.logs[0][1][0]
+    assert "value of key k is nan" in logger.logs[0][1][0]
 
 
 def test_csv_source_key_error():
