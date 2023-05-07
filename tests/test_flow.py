19aa679f546175d5587b639a88d7c9eb305a96d4
Tomer_Mamia <tomerm@iguazio.com>
2023-05-07 20:50:52 +0300

    change test_csv_reader_with_key_and_timestamp

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -355,9 +355,9 @@ def test_csv_reader_with_key_and_timestamp():
 
     assert len(termination_result) == 2
     assert termination_result[0].key == "m1"
-    assert termination_result[0].body == ["m1", datetime(2020, 2, 15, 2, 0), 8, True]
+    assert termination_result[0].body == {'b': True, 'k': 'm1', 't': datetime(2020, 2, 15, 2, 0), 'v': 8}
     assert termination_result[1].key == "m2"
-    assert termination_result[1].body == ["m2", datetime(2020, 2, 16, 2, 0), 14, False]
+    assert termination_result[1].body == {'b': False, 'k': 'm2', 't': datetime(2020, 2, 16, 2, 0), 'v': 14}
 
 
 @pytest.mark.parametrize(
