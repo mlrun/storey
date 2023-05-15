499457c317780577bd89e14285a73a4444580441
Tomer_Mamia <tomerm@iguazio.com>
2023-05-15 11:38:13 +0300

    change keys

--- storey/sources.py
+++ storey/sources.py
@@ -665,7 +665,7 @@ class DataframeSource(_IterableSource, WithUUID):
                     else:
                         line_id = self._get_uuid()
                     element = self._get_element(body, columns)
-                    keys = keys[0] if len(keys) == 1 else keys
+                    keys = keys[0] if len(keys) == 1 else (None if not keys else keys)
                     event = Event(element, keys, id=line_id)
                     await self._do_downstream(event)
         return await self._do_downstream(_termination_obj)
