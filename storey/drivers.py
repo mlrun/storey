43e8f4b531165c88ede3c4e430b9369d6ce7a663
Gal Topper <gal.topper@gmail.com>
2024-09-10 18:04:35 +0800

    Partially revert #532 (#535)
    Following https://github.com/v3io/v3io-py/pull/128
    
    [IG-23065](https://iguazio.atlassian.net/browse/IG-23065)

--- storey/drivers.py
+++ storey/drivers.py
@@ -532,7 +532,9 @@ class V3ioDriver(NeedsV3ioAccess, Driver):
 
         if use_parallel:
             for attr_name, d in pexpressions.items():
-                encoded_array = kv_array.encode_list(d["values"][d["first_index"] : d["last_index"] + 1])
+                encoded_array = kv_array.encode_list(d["values"][d["first_index"] : d["last_index"] + 1]).decode(
+                    "utf-8"
+                )
                 paggregate = self.parallel_aggregates[d["aggregation"]]
                 sliced_array = f'{attr_name}[{d["first_index"]}..{d["last_index"]}]'
                 expressions.append(f"{sliced_array}={paggregate}({sliced_array}, blob('{encoded_array}'))")
