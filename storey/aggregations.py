8ddd4399fb9dbffd6afa6b4bad5aa439a4599ede
David_Schneider <davesh0812@gmail.com>
2025-04-09 10:40:37 +0300

    .

--- storey/aggregations.py
+++ storey/aggregations.py
@@ -320,6 +320,7 @@ class QueryByKey(AggregateByKey):
     :param options: Enum flags specifying query options. (Optional)
     """
 
+
     def __init__(
         self,
         features: List[str],
