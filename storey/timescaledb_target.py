5a5cc322bf3d80b6c5f7024d31a461c81fd3db63
Alex Toker <alext@mckinsey.com>
2025-08-14 12:26:43 +0000

    Timescaledb: handle schemas

--- storey/timescaledb_target.py
+++ storey/timescaledb_target.py
@@ -112,6 +112,9 @@ class TimescaleDBTarget(_Batching, _Writer):
         self._dsn = dsn
         self._pool = None  # Connection pool will be created lazily during first use
         self._column_names = self._get_column_names()
+        self._schema = None
+        if "." in self._table:
+            self._schema, self._table = self._table.split(".", 1)
 
     def _init(self):
         """Initialize the target (called synchronously).
@@ -193,7 +196,9 @@ class TimescaleDBTarget(_Batching, _Writer):
         async with self._pool.acquire() as conn:
             # Use PostgreSQL's COPY protocol for optimal performance
             # This is significantly faster than individual INSERT statements
-            await conn.copy_records_to_table(self._table, records=records, columns=self._column_names)
+            await conn.copy_records_to_table(
+                self._table, schema_name=self._schema, records=records, columns=self._column_names
+            )
 
     async def _terminate(self):
         """Terminate and cleanup resources.
