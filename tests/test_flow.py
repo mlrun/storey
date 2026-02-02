554698306f9e5e3cad4dadcc8411b8e068c1f3e4
Tomer_Mamia <tomerm@iguazio.com>
2026-02-02 12:01:13 +0200

    .

--- tests/test_flow.py
+++ tests/test_flow.py
@@ -4016,6 +4016,7 @@ async def async_test_async_metadata_fields():
     assert result.key == "k1"
     assert result.body == body
 
+
 def test_async_metadata_fields():
     asyncio.run(async_test_async_metadata_fields())
 
